62f9c8bf0055c2a69f15eba158f53650cfcba576
Joseph <j.moorhouse1898@gmail.com>
2020-02-17 21:52:37 +0000

    Added local and global search tests

---
+++ pybees/utils/tests/test_combinatorial_local.py
@@ -0,0 +1,15 @@
+import pytest
+import numpy as np
+
+from pybees.utils.combinatorial_search import (
+    _random_choice_unique,
+    swap,
+    reversion,
+    insertion
+)
+
+# =============================================================================
+# Local search tests
+# =============================================================================
+
+    
