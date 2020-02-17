29174349f95c643a5fbaac5d1841bdc6ae587652
Joseph <j.moorhouse1898@gmail.com>
2020-02-17 22:11:18 +0000

    Added swap test

--- pybees/utils/tests/test_combinatorial_local.py
+++ pybees/utils/tests/test_combinatorial_local.py
@@ -12,4 +12,13 @@ from pybees.utils.combinatorial_search import (
 # Local search tests
 # =============================================================================
 
-    
+np.random.seed(0)
+
+def test_swap():
+    # simple test
+    bee_permutation = np.arange(10)
+    x = _random_choice_unique(np.arange(160).reshape(16, -1))
+
+    assert swap(bee_permutation, x.shape[0]) == x
+
+
