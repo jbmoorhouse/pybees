e2d36b9890fc4cd097fc24d12188caf9997c66bf
Joseph <j.moorhouse1898@gmail.com>
2020-02-18 15:01:49 +0000

    Added seoond swap test

--- pybees/utils/tests/test_combinatorial_local.py
+++ pybees/utils/tests/test_combinatorial_local.py
@@ -24,7 +24,20 @@ def test_swap():
     args = np.argwhere(res != bee_permutation)[:, 1].reshape(-1, 2)
     sol = np.array([[4, 9], [4, 6], [4, 6], [0, 4], [3, 7], [3, 5], [7, 9],
                     [5, 9], [2, 7], [7, 9], [4, 8], [3, 5], [6, 9], [2, 3],
-                    [1, 6],[4, 9]])
+                    [1, 6], [4, 9]])
+
+    assert np.array_equal(args, sol)
+
+    # simple test with np.random.seed(3)
+    # -------------------------------------------------------------------------
+    np.random.seed(3)
+    bee_permutation = np.arange(20)
+    res = swap(bee_permutation, 10)
+
+    # swapped arguments and known solution with np.random.seed(3)
+    args = np.argwhere(res != bee_permutation)[:, 1].reshape(-1, 2)
+    sol = np.array([[10, 16], [ 3, 15], [11, 13], [ 6, 15], [ 0,  2], [14, 19],
+                    [ 9, 19], [ 9, 13], [ 7, 15], [ 6,  7]])
 
     assert np.array_equal(args, sol)
 
