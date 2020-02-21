d0168445f41b66f97bfcc05523972ebe6ba31e85
Joseph <j.moorhouse1898@gmail.com>
2020-02-21 17:49:27 +0000

    Added test_instance

--- pybees/utils/tests/test_combinatorial_global.py
+++ pybees/utils/tests/test_combinatorial_global.py
@@ -1,2 +1,12 @@
 from pybees.utils.combinatorial_search import GlobalSearch
+from scipy.spatial.distance import cdist
+
+def test_instance():
+    np.random.seed(0)
+    arr = np.random.randint(10, size=[10, 2])
+
+    gs = GlobalSearch(arr)
+    
+    assert gs.coordinates == arr
+    assert gs.distance == cdist(arr, arr)
 
