e64ad009fd4bb90c90e446db75838c3922524ae9
Joseph <j.moorhouse1898@gmail.com>
2020-02-24 17:37:32 +0000

    Add imports

---
+++ pybees/__init__.py
@@ -0,0 +1,15 @@
+from pybees.bees_algorithm._simple_bees_algorithm import (
+    SimpleBeesContinuous,
+    SimpleBeesDiscrete
+)
+
+from pybees.utils.continuous_single_obj import (
+    levy,
+    ackley,
+    drop_wave,
+    de_jong,
+    easom,
+    michalewicz
+)
+
+from pybees.utils.combinatorial_single_obj import tour_distance
